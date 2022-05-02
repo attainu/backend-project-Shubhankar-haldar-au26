a32fd871dbb5ee95e5ced0acf9f7fff0c27a94d6
Shubhankar Haldar <84980055+Shubhankar-haldar-au26@users.noreply.github.com>
2022-05-02 10:01:11 +0530

    Delete routes directory

--- routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const MessageController = require("../controllers/MessageController");
-const router = require("express").Router();
-const JwtValidation = require("../middlewares/JwtValidation");
-
-router.post("/addmsg/", MessageController.addMessage);
-router.post("/getmsg/", MessageController.getMessages);
-
-module.exports = router;
